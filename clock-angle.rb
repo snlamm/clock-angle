9066e0bc7662e09c3c60cc93528227fed78b6531
snlamm <shmuel.lamm@gmail.com>
2016-02-25 06:50:34 -0500

    Update clock-angle.rb

--- clock-angle.rb
+++ clock-angle.rb
@@ -1,5 +1,5 @@
 
-# This method takes in an argument of a string (such as '4:30'), and converts the hour and minute
+# This code takes in an argument of a string (such as '4:30'), and converts the hour and minute
 # parts of it into integers. The until loop prompts for a response until a valid one is given. 
 # Valid hour and minute integers are then passed into variables that recongfigure them into
 # angles. Finally, the angles are subtracted from one another (depending on which is larger)
